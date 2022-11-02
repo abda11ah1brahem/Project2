a15a3db3b0c1d605f18dcc1390d922f94100e17e
Abdallah Ibrahem <112083552+abda11ah1brahem@users.noreply.github.com>
2022-11-02 11:17:29 +0200

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,13 @@
+class Employee:
+    def __init__(self, name, age, department, is_manager):
+        self.name = name
+        self.age = age
+        self.department = department
+        self.is_manager = is_manager
+class Student:
+    def __init__(self,name,age,gpa,major,is_graduated):
+        self.name = name
+        self.age = age
+        self.gpa = gpa
+        self.major = major
+        self.is_graduated = is_graduated
